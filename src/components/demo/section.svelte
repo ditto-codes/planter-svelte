cc189d9919d590da6f0636c55cfb9b4046e51605
mgluf <gluf.matt@gmail.com>
2021-12-29 22:16:44 -0500

    Demo Pages Content + Layout

---
+++ src/components/demo/section.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Text from '$components/text.svelte';
+  export let title = 'Section Title';
+  export let description = '';
+</script>
+
+<section>
+  <div class="heading">
+    <Text as="h2" class="h4">
+      {title}
+    </Text>
+  </div>
+  {#if description}
+    <p class="description">
+      {description}
+    </p>
+  {/if}
+  <slot/>
+</section>
+
+<style lang="scss">
+
+  section {
+    margin-bottom: sp(xxl);
+  }
+
+  .heading {
+    padding-bottom: sp(1.25);
+    margin-bottom: sp(md);
+    border-bottom: solid $gray 1px;
+    :global(*) {
+      padding: 0;
+      margin: 0;
+    }
+  }
+
+  .description {
+    margin-bottom: sp(lg);
+  }
+
+</style>
---
+++ src/components/demo/sidebar.svelte
@@ -0,0 +1,98 @@
+<!--
+  
+  SIDEBAR
+  
+  Sidebar for demo organization.
+  
+-->
+<script>  
+  export let { class: className } = '';
+  export let links = {
+    'Components': [
+      'Button',
+      'Text',
+      'Link',
+      'Image',
+      'Icon',
+      'Nav',
+    ],
+    'Tokens': [
+      'Typography',
+      'Color',
+    ]
+  };
+
+</script>
+
+<!-- RENDER
+────────────────────────────────── -->
+<nav>
+  
+  <ul>
+    {#each Object.keys(links) as heading}
+      <li class="section">
+        <p class="heading h6">{heading}</p>
+        <ul>
+          {#each links[heading] as link}
+            <li>
+              <a href={`/demo/${link.toLowerCase()}`} class="link">{link}</a>
+            </li>
+          {/each}
+        </ul>
+      </li>
+    {/each}
+  </ul>
+</nav>
+
+
+<!-- STYLES
+────────────────────────────────── -->
+<style lang="scss">
+  nav {
+    background-color: $canvas;
+    border-bottom: 1px solid $gray-100;
+    padding: 0 sp(lg);
+    width: 200px;
+
+    ul {
+      margin: 0;
+      padding: 0;
+    }
+
+    li {
+      padding: sp(xs) sp(sm);
+      list-style: none;
+      // display: inline-block;
+    }
+  }
+
+  .section {
+    padding: 0;
+  }
+
+  .heading {
+    padding: sp(xl) 0 sp(sm);
+    margin: 0;
+  }
+
+  .link {
+    text-decoration: none;
+    display: inline-block;
+    width: 100%;
+    padding: sp(xs sm);
+    border-radius: 2px;
+    color: $gray;
+    transition: $animate-fast;
+
+    &:hover {
+      color: $blue;
+      background-color: rgba($blue-100, .25);
+    }
+
+    &.selected {
+      color: $blue;
+      background-color: rgba($blue, .5);
+    }
+  }
+</style>
+
