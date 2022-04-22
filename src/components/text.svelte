e386cef7e262abfcc9d4695c3902817028257c93
mgluf <gluf.matt@gmail.com>
2022-04-21 20:27:36 -0400

    updated text.svelte with :element directive

--- src/components/text.svelte
+++ src/components/text.svelte
@@ -7,7 +7,7 @@
 -->
 
 <script>
-  export let as = '';
+  export let as = 'p';
   let className = '';
   export { className as class };
 </script>
@@ -15,20 +15,6 @@
 <!-- RENDER
 ────────────────────────────────── -->
 
-{#if as === 'h1'}
-  <h1 class={className}><slot/></h1>
-{:else if as === 'h2'}
-  <h2 class={className}><slot/></h2>
-{:else if as === 'h3'}
-  <h3 class={className}><slot/></h3>
-{:else if as === 'h4'}
-  <h4 class={className}><slot/></h4>
-{:else if as === 'h5'}
-  <h5 class={className}><slot/></h5>
-{:else if as === 'h6'}
-  <h6 class={className}><slot/></h6>
-{:else if as === 'small'}
-  <small class={className}><slot/></small>
-{:else}
-  <p class={className}><slot/></p>
-{/if}
+<svelte:element this={as} class={className}>
+  <slot/>
+</svelte:element>
